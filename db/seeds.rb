692cf187168e3cc6e7d288d28caf0072fb74ec28
Quentin Riviere <quentin.l.riviere@gmail.com>
2018-11-19 16:41:08 +0100

    Seeds Done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Clothe.destroy_all
+Rental.destroy_all
+User.destroy_all
+
+puts 'Crating users'
+pierre = User.create(first_name: 'Pierre', last_name: "Kiroul", address: "Lyon", email: "pierrot@gmail.com", password: "azerty")
+louis = User.create(first_name: 'Louis', last_name: "Labrocante", address: "Paris", email: "louis@gmail.com", password: "azerty")
+arthur = User.create(first_name: 'Arthur', last_name: "john", address: "Marseille", email: "arthur@gmail.com", password: "azerty")
+puts "done"
+
+p pierre
+p pierre.id
+
+puts "Creating Clothes"
+tee_shirt = Clothe.new(name: 'Tee-shirt', size: "XXL", brand: "Lafuma", state: "4", owner_id: pierre.id)
+p tee_shirt.save!
+pull = Clothe.new(name: 'Pull', size: "XS", brand: "Sergio", state: "2", owner_id: louis.id)
+p pull.save!
+chaussure = Clothe.new(name: 'Chaussure', size: "32", brand: "Lacoste", state: "5", owner_id: arthur.id)
+p chaussure.save!
+puts "done"
+
+puts "Creating Rentals"
+rental1 = Rental.create(renter_id: pierre.id, clothe_id: chaussure.id)
+rental2 = Rental.create(renter_id: louis.id, clothe_id: chaussure.id)
+rantal3 = Rental.create(renter_id: arthur.id, clothe_id: tee_shirt.id)
+puts "done"
+
+
