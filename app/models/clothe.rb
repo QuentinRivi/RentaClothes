e9765dbcbccf8113b03b218b503c92a53aab9cce
Emmanuelle Nadal <manue.nadal@gmail.com>
2018-11-21 13:57:59 +0100

    added design for notifications

--- app/models/clothe.rb
+++ app/models/clothe.rb
@@ -2,4 +2,8 @@ class Clothe < ApplicationRecord
   belongs_to :owner, class_name: "User", foreign_key: "owner_id"
   has_many :renters, through: :rentals
   has_one_attached :photo
+
+  def img_thumbnail
+    cl_image_tag(photo, width: 200, crop: "scale")
+  end
 end
