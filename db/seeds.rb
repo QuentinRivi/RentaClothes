d109a33040915e8086acd7974f72c56d8df2c1f4
Quentin Riviere <quentin.l.riviere@gmail.com>
2018-11-22 16:43:54 +0100

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Notification.destroy_all
-Rental.destroy_all
-Clothe.destroy_all
-User.destroy_all
 
 puts 'Creating users'
 manue = User.create(first_name: 'Pierre', last_name: "Kiroul", address: "Lyon", email: "pierrot@gmail.com", password: "azerty")
