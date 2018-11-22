e3d677e1bb570d3196e4f3a16c6a94c510e419b3
Delbosco <delbosco.arthur@gmail.com>
2018-11-22 18:52:05 +0100

    modification of index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,8 +11,8 @@ Clothe.destroy_all
 User.destroy_all
 
 puts 'Creating users'
-pierre = User.create(first_name: 'Pierre', last_name: "Kiroul", address: "Lyon", email: "pierrot@gmail.com", password: "azerty")
-louis = User.create(first_name: 'Louis', last_name: "Labrocante", address: "Paris", email: "louis@gmail.com", password: "azerty")
+manue = User.create(first_name: 'Emmanuelle', last_name: "Nadal", address: "Lyon", email: "manue.nadal@gmail.com", password: "azerty")
+quentin = User.create(first_name: 'Quentin', last_name: "Rivière", address: "Paris", email: "quentin.l.riviere@gmail.com", password: "azerty")
 
 arthur = User.create(first_name: 'Arthur', last_name: "john", address: "Marseille", email: "arthur@gmail.com", password: "azerty")
 puts "done"
