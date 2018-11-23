da9f7cc0fe6df65cebeecf9b06b4e977f98d7d1c
Quentin Riviere <quentin.l.riviere@gmail.com>
2018-11-23 11:54:19 +0100

    Please merge me

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,6 @@
 import "bootstrap";
 import { loadDynamicBannerText } from '../packs/banner.js';
 import { initUpdateNavbarOnScroll } from '../packs/navbar.js';
+import "./flashes";
 initUpdateNavbarOnScroll();
 loadDynamicBannerText();
