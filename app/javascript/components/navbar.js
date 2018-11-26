b3fd31571b5fc3e9c02630499670426007031b5d
Quentin Riviere <quentin.l.riviere@gmail.com>
2018-11-26 14:58:58 +0100

    dome

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,3 +1,20 @@
+function initUpdateNavbarOnScroll() {
+  const navbar = document.querySelector('.navbar-wagon');
+  const navbar2 = document.getElementById('home-h4');
+  if (navbar && navbar2) {
+    console.log(navbar)
+    window.addEventListener('scroll', () => {
+      if (window.scrollY >= window.innerHeight) {
+        navbar.classList.add('navbar-wagon-white');
+      } else {
+        navbar.classList.remove('navbar-wagon-white');
+      }
+    });
+  }
+}
+
+export { initUpdateNavbarOnScroll };
+
 const updateBadge = (number) => {
   const badge = document.querySelector('.navbar-wagon .badge');
 
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,7 @@
 import "bootstrap";
 import '../components/navbar.js';
 import { loadDynamicBannerText } from '../packs/banner.js';
-import { initUpdateNavbarOnScroll } from '../packs/navbar.js';
+import { initUpdateNavbarOnScroll } from '../components/navbar.js';
 import "./flashes";
 initUpdateNavbarOnScroll();
 loadDynamicBannerText();
